84673c75999e6eb70fbc376c0d6ea8338802fccc
Andrey Popov <andrvpopov@gmail.com>
2022-01-27 08:05:39 +0300

    Make db-apps branch

--- Task02/cold-hot/src/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	namespace Ecarus1\coldhot\Controller;
-    use function Ecarus1\coldhot\View\showGame;
-
-    function startGame() {
-        echo "Game started".PHP_EOL;
-        showGame();
-    }
-?> 
--- Task02/cold-hot/src/View.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	namespace Ecarus1\coldhot\View;
-    function showGame() {
-        echo "Game shown";
-    }
-?> 
